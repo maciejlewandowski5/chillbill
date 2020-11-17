f929f4dda643bbbb8563acfdeca278aa150bc281
maciejlewandowski5 <64683954+maciejlewandowski5@users.noreply.github.com>
2020-11-17 12:27:06 +0100

    History item list now has a bar showing content of catoegorys

--- app/src/main/java/com/example/chillbill/HistoryItem.java
+++ app/src/main/java/com/example/chillbill/HistoryItem.java
@@ -1,9 +1,13 @@
 package com.example.chillbill;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,25 +33,24 @@ public class HistoryItem extends Fragment {
     ProgressBar green;
     ProgressBar orange;
     ProgressBar blue;
+    float purpleFloat;
+    float yellowFloat;
+    float greenFloat;
+    float orangeFloat;
+    float blueFloat;
+
 
     public HistoryItem() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HistoryItem.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HistoryItem newInstance(float purpleCategory,float yellowCategory,float greenCategory,float orangeCategory,float blueCategory) {
+
+    public static HistoryItem newInstance(float purpleCategory, float yellowCategory, float greenCategory, float orangeCategory, float blueCategory) {
         HistoryItem fragment = new HistoryItem();
         Bundle args = new Bundle();
 
 
+        // Shoud be in percentage where 100% is purpleFloat + ... + blueFloat
         args.putFloat("purpleFloat", purpleCategory);
         args.putFloat("yellowFloat", yellowCategory);
         args.putFloat("greenFloat", greenCategory);
@@ -62,26 +65,89 @@ public class HistoryItem extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        float sum =0
+        float sum = 0;
 
+        // 268 dp is 300dp(historyItem Width) - 16dp*2(for margins start and end)
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-           sum =  getArguments().getFloat("purpleFloat")+getFloat("purpleFloat")+getFloat("purpleFloat")+getFloat("purpleFloat")+getFloat("purpleFloat");
+            purpleFloat = 268.0f*getArguments().getFloat("purpleFloat")/100.0f;
+            yellowFloat = 268.0f*getArguments().getFloat("yellowFloat")/100.0f;
+            greenFloat = 268.0f*getArguments().getFloat("greenFloat")/100.0f;
+            orangeFloat = 268.0f*getArguments().getFloat("orangeFloat")/100.0f;
+            blueFloat = 268.0f*getArguments().getFloat("blueFloat")/100.0f;
+            sum = purpleFloat + yellowFloat + greenFloat + orangeFloat + blueFloat;
         }
-        purple = getView().findViewById(R.id.progressBar5);
-        yellow = getView().findViewById(R.id.progressBar4);
-        green = getView().findViewById(R.id.progressBar2);
-        orange = getView().findViewById(R.id.progressBar3);
-        blue = getView().findViewById(R.id.progressBar7);
 
 
     }
 
+    // 268dp is total width of all progress bars
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+        View RootView = inflater.inflate(R.layout.fragment_history_item, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_item, container, false);
+        purple = (ProgressBar) RootView.findViewById(R.id.progressBar5);
+        yellow = (ProgressBar) RootView.findViewById(R.id.progressBar4);
+        green = (ProgressBar) RootView.findViewById(R.id.progressBar2);
+        orange = (ProgressBar) RootView.findViewById(R.id.progressBar3);
+        blue = (ProgressBar) RootView.findViewById(R.id.progressBar7);
+
+
+        float heightDp = 10f;
+        float heightPx = DptoPx(heightDp);
+        ConstraintLayout constraintLayout = RootView.findViewById(R.id.constrainViewHistoryFragment);
+
+        /// Purple
+        purple.setLayoutParams(new ConstraintLayout.LayoutParams((int) DptoPx(purpleFloat),(int)heightPx));
+        ConstraintSet purpleConstrainSet = new ConstraintSet();
+        purpleConstrainSet.clone(constraintLayout);
+        purpleConstrainSet.connect(purple.getId(),ConstraintSet.BOTTOM,ConstraintSet.PARENT_ID,ConstraintSet.BOTTOM);
+        purpleConstrainSet.connect(purple.getId(),ConstraintSet.START,ConstraintSet.PARENT_ID,ConstraintSet.START,DptoPx(16.0f));
+        purpleConstrainSet.applyTo(constraintLayout);
+
+        /// Yellow
+        yellow.setLayoutParams(new ConstraintLayout.LayoutParams((int) DptoPx(yellowFloat), (int) heightPx));
+        ConstraintSet yellowConstraintSet = new ConstraintSet();
+        yellowConstraintSet.clone(constraintLayout);
+        yellowConstraintSet.connect(yellow.getId(),ConstraintSet.BOTTOM,ConstraintSet.PARENT_ID,ConstraintSet.BOTTOM);
+        yellowConstraintSet.connect(yellow.getId(),ConstraintSet.START,purple.getId(),ConstraintSet.END);
+        yellowConstraintSet.applyTo(constraintLayout);
+
+        /// Green
+        green.setLayoutParams(new ConstraintLayout.LayoutParams((int) DptoPx(greenFloat), (int) heightPx));
+        ConstraintSet greenConstraintSet = new ConstraintSet();
+        greenConstraintSet.clone(constraintLayout);
+        greenConstraintSet.connect(green.getId(),ConstraintSet.BOTTOM,ConstraintSet.PARENT_ID,ConstraintSet.BOTTOM);
+        greenConstraintSet.connect(green.getId(),ConstraintSet.START,yellow.getId(),ConstraintSet.END);
+        greenConstraintSet.applyTo(constraintLayout);
+
+        /// Orange
+        orange.setLayoutParams(new ConstraintLayout.LayoutParams((int) DptoPx(orangeFloat), (int) heightPx));
+        ConstraintSet orangeConstraintSet = new ConstraintSet();
+        orangeConstraintSet.clone(constraintLayout);
+        orangeConstraintSet.connect(orange.getId(),ConstraintSet.BOTTOM,ConstraintSet.PARENT_ID,ConstraintSet.BOTTOM);
+        orangeConstraintSet.connect(orange.getId(),ConstraintSet.START,green.getId(),ConstraintSet.END);
+        orangeConstraintSet.applyTo(constraintLayout);
+
+        /// Blue
+        blue.setLayoutParams(new ConstraintLayout.LayoutParams((int) DptoPx(blueFloat), (int) heightPx));
+        ConstraintSet blueConstraintSet = new ConstraintSet();
+        blueConstraintSet.clone(constraintLayout);
+        blueConstraintSet.connect(blue.getId(),ConstraintSet.BOTTOM,ConstraintSet.PARENT_ID,ConstraintSet.BOTTOM);
+        blueConstraintSet.connect(blue.getId(),ConstraintSet.START,orange.getId(),ConstraintSet.END);
+        blueConstraintSet.applyTo(constraintLayout);
+
+
+        return RootView;
+
+
+    }
+
+    int DptoPx(float dp){
+        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
     }
 }
--- app/src/main/java/com/example/chillbill/StartScreen.java
+++ app/src/main/java/com/example/chillbill/StartScreen.java
@@ -2,7 +2,10 @@ package com.example.chillbill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.synnapps.carouselview.CarouselView;
@@ -18,6 +21,18 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        HistoryItem first = HistoryItem.newInstance(20,10,20,40,10);
+        HistoryItem second = HistoryItem.newInstance(10,20,30,30,10);
+        HistoryItem third = HistoryItem.newInstance(5,25,35,20,15);
+
+
+        androidx.fragment.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment2,first);
+        transaction.replace(R.id.fragment3,second);
+        transaction.replace(R.id.fragment4,third);
+        transaction.commit();
 
         carouselView = (CarouselView) findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
